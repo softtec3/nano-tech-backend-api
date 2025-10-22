c1d13cbb0e02e992af7e73b6b6e5f99ec51b6fdb
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-10-22 09:35:50 +0600

    update db connect with dynamic origin

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,17 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// specific for fetch credential include
-header("Access-Control-Allow-Origin: http://localhost:5173"); // your frontend URL
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json; charset=UTF-8");
+$allowed_origins = [
+    "http://localhost:5173",               // local development (Vite)
+    "https://admin.nano-techbd.com",       // production frontend
+];
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
 }
+
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=utf-8");
 
 $host = "localhost";
 $username = "softtec3_nano_admin";
