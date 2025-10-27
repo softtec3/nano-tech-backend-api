8938548563aa4d8a3a344191de13c72c0d6d5d0c
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-10-27 09:39:33 +0600

    update user_signup

--- front/user_signup.php
+++ front/user_signup.php
@@ -22,6 +22,27 @@ try {
         throw new Exception("Error creating general_users: " . $conn->error);
     }
 
+    $sql2 = "CREATE TABLE IF NOT EXISTS users_information (
+	id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int NOT NULL,
+    user_name varchar(100) UNIQUE NOT NULL,
+    full_name varchar(255) DEFAULT NULL,
+    image TEXT DEFAULT NULL,
+    mobile_number varchar(20) DEFAULT NULL,
+    address_label ENUM('home','office','other') DEFAULT 'home',
+    area varchar(255) DEFAULT NULL,
+    address TEXT DEFAULT NULL,
+    landmark TEXT DEFAULT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES general_users(id) ON UPDATE CASCADE ON DELETE CASCADE
+    )";
+    if (!$conn->query($sql2)) {
+        throw new Exception("Error creating users_information: " . $conn->error);
+    }
+
+
+
     if ($_SERVER["REQUEST_METHOD"] != "POST") {
         throw new Exception("Invalid request method. Must be POST request");
     }
@@ -47,6 +68,17 @@ try {
         throw new Exception("Failed to insert " . $stmt->error);
     }
     if ($stmt->insert_id) {
+        $id = $stmt->insert_id;
+        $stmt2 = $conn->prepare("INSERT INTO users_information(user_id,user_name, full_name) VALUES (?,?,?)");
+        if (!$stmt2) {
+            throw new Exception("SQL failed users_information: " . $conn->error);
+        }
+        $stmt2->bind_param("iss", $id, $user_name, $full_name);
+
+        if (!$stmt2->execute()) {
+            throw new Exception("Insert failed to users_information: " . $stmt2->error);
+        }
+
         $response["success"] = true;
         $response["message"] = "Signup successful";
         $response["data"] = ["insert_id" => $stmt->insert_id];
