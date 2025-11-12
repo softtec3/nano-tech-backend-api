6d37e0651adbe9e3529be9ea2a36b19d1fbc2e17
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-11-12 14:22:50 +0600

    added all transactions endpoint

---
+++ all_transactions.php
@@ -0,0 +1,47 @@
+<?php
+require_once("./db_connect.php");
+require_once("./auth_admin_only.php");
+$response = [
+    "success" => false,
+    "message" => "",
+    "data" => []
+];
+
+
+try {
+    if ($_SERVER["REQUEST_METHOD"] != "GET") {
+        throw new Exception("Invalid request method. Must be GET request");
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM transactions ORDER BY id DESC");
+    if (!$stmt) {
+        throw new Exception("SQL failed: " . $conn->error);
+    }
+
+    if (!$stmt->execute()) {
+        throw new Exception("Fetching failed; " . $stmt->error);
+    }
+
+    $result = $stmt->get_result();
+    $transactions = [];
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $transactions[] = $row;
+        }
+        $response["success"] = true;
+        $response["message"] = "Fetching successful";
+        $response["data"] = $transactions;
+    } else {
+        $response["success"] = true;
+        $response["message"] = "Fetching successful";
+        $response["data"] = [];
+    }
+
+    $stmt->close();
+    $conn->close();
+} catch (Exception $e) {
+    $response["success"] = false;
+    $response["message"] = $e->getMessage();
+} finally {
+    echo json_encode($response);
+}
